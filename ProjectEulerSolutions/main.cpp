02e0bfb7745f453d58ba56f9c9d4383dac4688a7
Andrew Sansom <qthequartermasterman@gmail.com>
2019-02-11 13:34:57 -0600

    Initial Commit

---
+++ ProjectEulerSolutions/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjectEulerSolutions
+//
+//  Created by Andrew Sansom on 2/11/19.
+//  Copyright © 2019 Andrew Sansom. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
