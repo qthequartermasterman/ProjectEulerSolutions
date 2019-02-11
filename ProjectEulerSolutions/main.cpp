f07dccc6f62e81481e94b1be346bdb2e0757b2c5
Andrew Sansom <qthequartermasterman@gmail.com>
2019-02-11 15:47:46 -0600

    Initialized project and included solution for problem 1.

--- ProjectEulerSolutions/main.cpp
+++ ProjectEulerSolutions/main.cpp
@@ -7,9 +7,10 @@
 //
 
 #include <iostream>
+#include "problem1.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << problem1() << std::endl;
     return 0;
 }
---
+++ ProjectEulerSolutions/problem1.h
@@ -0,0 +1,26 @@
+//
+//  problem1.h
+//  ProjectEulerSolutions
+//
+//  Created by Andrew Sansom on 2/11/19.
+//  Copyright © 2019 Andrew Sansom. All rights reserved.
+//
+
+#ifndef problem1_h
+#define problem1_h
+
+/* The problem asks for the sum of all numbers less than 1000 that are multiples of 3 or 5. */
+int problem1(){
+    int runningSum = 0;
+    for (int i = 0; i < 1000;i+=3){
+        runningSum+=i;
+    }
+    for (int i = 0; i < 1000; i+=5){
+        if (i%15 != 0){
+            runningSum+=i;
+        }
+    }
+    return runningSum;
+}
+
+#endif /* problem1_h */
