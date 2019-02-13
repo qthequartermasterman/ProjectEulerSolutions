0c62b3f074d01918e084b3bc421eee3419f56d83
Andrew Sansom <qthequartermasterman@gmail.com>
2019-02-12 18:35:52 -0600

    Improved solution for Problem 2.

--- ProjectEulerSolutions/problem2.h
+++ ProjectEulerSolutions/problem2.h
@@ -9,25 +9,22 @@
 #ifndef problem2_h
 #define problem2_h
 
+#include <cmath>
 
-int getNextFibonacciNumber(int number1, int number2){
-    return number1 + number2;
+int getNextFibonacciNumber(int number){
+    // We can calculate the next Fibonacci number in the sequence by multiplying the previous term by the golden ratio and rounding to the nearest integer.
+    const float phi = (1+sqrt(5))/2;
+    return (int) floor(number * phi+0.5);
 }
-
 
 /* Problem asks for the sum of all even fibonacci numbers less than 4 million*/
 int problem2(){
     int runningSum = 2;
-    int number1 = 1;
-    int number2 = 2;
-    
-    while (number2 < 4000000){
-        int nextNumber = getNextFibonacciNumber(number1, number2);
-        number1=number2;
-        number2=nextNumber;
-        
-        if (nextNumber%2 == 0){
-            runningSum+=nextNumber;
+    int number = 2;
+    while (number < 4000000){
+        number = getNextFibonacciNumber(number);
+        if (number%2==0){
+            runningSum+=number;
         }
     }
     return runningSum;
